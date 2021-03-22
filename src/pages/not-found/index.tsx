3c8b3fae405eda387eb2f1a89fc87632d9979b21
Héctor Francés <32682445+hfrances@users.noreply.github.com>
2021-03-22 21:32:38 +0100

    Check gh-pages

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -4,6 +4,7 @@ const NotFoundPage = (): JSX.Element => {
   return (
     <MainFrame headerTitle="Page not found" footerVisible={true}>
       <div>Not found</div>
+      <div>:'(</div>
     </MainFrame>
   );
 }
