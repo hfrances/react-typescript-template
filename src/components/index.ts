423c9306ac6fca76b7e6da4f9d5257b6dd86803f
Héctor Francés <32682445+hfrances@users.noreply.github.com>
2021-03-26 09:29:32 +0100

    Rename AlertRectangle component.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,2 @@
 export * from './main-frame'
 export * from './alert-rectangle';
-export * from './alert-rectangle-stored';
--- src/components/main-frame/index.tsx
+++ src/components/main-frame/index.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent, Fragment, useState } from 'react';
 import CSS from 'csstype';
-import { AlertRectangleStored } from '../';
+import { AlertRectangle } from '../';
 import Header from './header';
 import Footer from './footer';
 
@@ -21,7 +21,7 @@ const MainFrame: FunctionComponent<MainFrameProps> = ({ headerTitle, contentType
   return (
     <Fragment>
       <Header title={headerTitle} onHeight={setHeaderHeight} />
-      <AlertRectangleStored style={{ marginTop: headerHeight }} />
+      <AlertRectangle style={{ marginTop: headerHeight }} />
       <main id="main-container" className={`App-container ${contentType ?? ''}`.trimEnd()} style={{ marginTop: headerHeight, marginBottom: footerHeight, ...contentStyle }}>
         {children}
       </main>
