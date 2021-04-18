38e8655b08c2bc651c4f828e58ff871b93a1448c
Héctor Francés <32682445+hfrances@users.noreply.github.com>
2021-04-18 11:15:30 +0200

    Add fixedList property to MenuPage component

--- src/components/navigation-bar/index.tsx
+++ src/components/navigation-bar/index.tsx
@@ -7,10 +7,11 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 type NavigationBarProps = {
   drawerWidth?: number;
   list?: React.ReactNode;
+  fixedList?: React.ReactNode;
   style?: React.CSSProperties;
 }
 
-const NavigationBar: FunctionComponent<NavigationBarProps> = ({ drawerWidth, list, style, children }) => {
+const NavigationBar: FunctionComponent<NavigationBarProps> = ({ drawerWidth, list, fixedList, style, children }) => {
   const classes = useStyles();
   const drawerClasses = makeStyles((theme) => ({
     width: {
@@ -29,18 +30,23 @@ const NavigationBar: FunctionComponent<NavigationBarProps> = ({ drawerWidth, lis
         [classes.drawerOpen]: open, [drawerClasses.width]: open,
         [classes.drawerClose]: !open,
       })}`}>
-        <div style={{ overflowY: 'auto', overflowX: 'hidden' }}>
+        <div className={classes.navBarContent}>
           {list}
         </div>
         <Divider />
-        <div className={`${classes.navBarButton} ${clsx({ [classes.drawerButtonOpen]: open, [classes.drawerButtonClose]: !open, })}`}>
-          <IconButton onClick={handleDrawerClose}>
-            {open ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
+        <div className={classes.navBarFixed}>
+          <div className={classes.navBarFixedContent}>
+            {fixedList}
+          </div>
+          <div className={`${classes.navBarButton} ${clsx({ [classes.drawerButtonOpen]: open, [classes.drawerButtonClose]: !open, })}`}>
+            <IconButton onClick={handleDrawerClose}>
+              {open ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+          </div>
         </div>
       </nav>
-      <div className="nav-content" style={{ display: 'grid', overflowY: 'auto', width: 'inherit' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+      <div className={classes.navContainer}>
+        <div className={classes.navContent}>
           {children}
         </div>
       </div>
@@ -71,11 +77,28 @@ const useStyles = makeStyles((theme) => ({
     flexShrink: 0,
     whiteSpace: 'nowrap'
   },
-  navBarButton: {
+  navBarContent: {
+    overflowY: 'auto',
+    overflowX: 'hidden'
+  },
+  navBarFixed: {
     display: 'flex',
     flex: 'auto',
+    flexDirection: 'column',
     justifyContent: 'flex-end',
-    alignItems: 'flex-end',
+  },
+  navBarFixedContent: {
+    marginBottom: '-15px', 
+    overflowX: 'hidden'
+  },
+  navBarButton: {
+    display: 'flex',
+  },
+  navContainer: {
+    display: 'grid', overflowY: 'auto', width: 'inherit'
+  },
+  navContent: {
+    display: 'flex', flexDirection: 'column', justifyContent: 'center'
   },
   drawerOpen: {
     transition: theme.transitions.create('width', {
@@ -96,8 +119,10 @@ const useStyles = makeStyles((theme) => ({
   },
   drawerButtonOpen: {
     justifyContent: 'flex-end',
+    marginRight: '4px'
   },
   drawerButtonClose: {
     justifyContent: 'flex-start',
+    marginLeft: '4px'
   }
 }));
--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,12 +1,22 @@
 import { useEffect } from 'react';
 import { MainFrame, NavigationBar } from '../../components'
 import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import SettingsIcon from '@material-ui/icons/Settings';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import PhotoIcon from '@material-ui/icons/Photo';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import AccountTreeIcon from '@material-ui/icons/AccountTree';
 import debug from '../../helpers/debug';
 
 const MenuPage = (): JSX.Element => {
-  const icons = [<InboxIcon />, <MailIcon />];
+  const icons = [
+    <AddShoppingCartIcon/>, <InboxIcon />, <MailIcon />, 
+    <CloudUploadIcon/>, <PhotoIcon/>, <AccountBalanceIcon/>, <AccountTreeIcon/>
+  ];
 
   const generate = (count: number, element: (item: number) => JSX.Element): JSX.Element[] | undefined => {
     let elements = new Array<JSX.Element>(count);
@@ -27,13 +37,24 @@ const MenuPage = (): JSX.Element => {
         <List>
           {generate(10, item =>
             <ListItem button key={item}>
-              <ListItemIcon>{icons[item % icons.length]}</ListItemIcon>
-              <ListItemText primary={`menu item ${item}`} />
+              <ListItemIcon>{icons[(item-1) % icons.length]}</ListItemIcon>
+              <ListItemText primary={`menu-item-${item}`} />
             </ListItem>
           )}
         </List>
+      } fixedList={
+        <List>
+          <ListItem button>
+            <ListItemIcon><AccountCircleIcon /></ListItemIcon>
+            <ListItemText primary="Account" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon><SettingsIcon /></ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItem>
+        </List>
       }>
-        {generate(10, item =>
+        {generate(15, item =>
           <div key={`c${item}`}>{`content ${item}`}</div>
         )}
       </NavigationBar>
